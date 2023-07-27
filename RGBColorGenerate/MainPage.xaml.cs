677111a1fde95b67fffe9043f31b4e5874d95bbc
Alfredo Sánchez <45853187+AlfredoSV@users.noreply.github.com>
2023-07-27 11:42:35 -0600

    Add Slider_ValueChanged, SetColor and GenerateColor_Clicked

--- RGBColorGenerate/MainPage.xaml.cs
+++ RGBColorGenerate/MainPage.xaml.cs
@@ -9,5 +9,27 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
+	public void GenerateColor_Clicked(object sender, EventArgs e)
+	{
+
+	}
+
+    private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+		double colorRed = Slider_Red.Value;
+        double colorBlue = Slider_Blue.Value;
+        double colorGreen = Slider_Green.Value;
+
+		Color color = Color.FromRgb(colorRed, colorGreen, colorBlue);
+        SetColor(color);
+    }
+
+    private void SetColor(Color color)
+    {
+        Grid_Color.Background = color;
+        BtnGenerateColor.BackgroundColor = color;
+        ResultColor.Text = color.ToHex();
+    }
+
 }
 
